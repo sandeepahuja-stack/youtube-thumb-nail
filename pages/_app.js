08666914fa009bb6a3e3a49148f849cb03786154
Devanshu Malhota <43911041+devanshu23598@users.noreply.github.com>
2021-08-29 17:16:51 +0530

    Update _app.js
    Ga event script added

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,15 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <div dangerouslySetInnerHTML={{__html:`<!-- Global site tag (gtag.js) - Google Analytics --> <script async src="https://www.googletagmanager.com/gtag/js?id=UA-205726605-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-205726605-1');
+        </script>
+      `}}/>
       <style>
         {`
         .bg{
